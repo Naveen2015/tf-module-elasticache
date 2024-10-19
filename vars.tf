0aa17573d12cc852ffd94340b66f26dc2a921c47
naveen <naveenkumar9707@gmail.com>
2024-10-19 13:50:39 -0500

    elasticache

---
+++ vars.tf
@@ -0,0 +1,17 @@
+variable "subnets" {}
+variable "name" {
+  default = "elasitcache"
+}
+variable "tags" {}
+variable "env" {}
+variable "vpc_id" {}
+variable "allow_db_cidr" {}
+variable "engine_version" {}
+variable "kms_arn" {}
+variable "port_no" {
+  default = 6379
+}
+variable "node_type" {}
+variable "instance_class" {}
+variable "replicas_per_node_group" {}
+variable "num_node_groups" {}
